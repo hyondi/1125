2c32a2be52676582413f2fd38d29a09c4530b81c
hyondi <haepari0428@duksung.ac.kr>
2022-11-25 17:46:12 +0900

    수정

---
+++ www/sketch.js
@@ -0,0 +1,27 @@
+var wave;
+
+function setup(){
+  createCanvas (720, 256);
+  wave = new p5.Oscillator();
+  wave.setType('sine');
+  // wave.start();
+  wave.freq(440); /*음정*/
+  wave.amp(0); /*볼륨*/
+
+  button = createButton('play/pause');
+  button.mousePressed(toggle);
+}
+
+function draw(){
+}
+
+function toggle(){
+  if (!playing) {
+    wave.start();
+    wave.amp(0.5, 1);
+    playing = true;
+  } else {
+    wave.amp(0,1);
+    playing = false;
+  }
+  }
