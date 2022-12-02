c37516834ea5d28c0d4101cc2bc5d1f135db89a2
hyondi <haepari0428@duksung.ac.kr>
2022-12-02 16:28:43 +0900

    skecth2
    ㅇㅇ

--- sketch.js
+++ sketch.js
@@ -1,27 +1,35 @@
 var wave;
+var button;
 var playing = false;
-function setup(){
-  createCanvas (720, 256);
+function setup() {
+  createCanvas(720,256);
   wave = new p5.Oscillator();
   wave.setType('sine');
-  // wave.start();
-  wave.freq(440); /*음정*/
-  wave.amp(0); /*볼륨*/
-
+  wave.freq(440);
+  wave.amp(0);
+  
+    
   button = createButton('play/pause');
   button.mousePressed(toggle);
 }
 
-function draw(){
+function draw() {
+  wave.freq(ampValue);
 }
 
 function toggle(){
-  if (!playing) {
+  if(!playing){
     wave.start();
-    wave.amp(0.5, 1);
-    playing = true;
-  } else {
-    wave.amp(0,1);
+      wave.amp(0.3, 1);
+      playing = true;
+  }else {
+    ampValue =0;
     playing = false;
   }
-  }
+}
+  
+function devicedMoved(){
+  ampValue = accelerationX*1000;
+  background(255,255,255);
+  text(accelerationX*2, 10, 10);
+}
